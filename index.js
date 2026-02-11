17154ee153a1f3c61ff4a7903db1fd786f2d64fd
ved04151 <suh04151@gmail.com>
2026-02-11 11:47:30 +0530

    setup basic express server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("server starting...");
+// console.log("server starting...");
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) =>{
+    res.send("Api is running...");
+})
+
+const PORT = 5000;
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
